3457dc9353a7b9ead27bb3911bed6edd2da41b17
pedro <pedro.pereira@edu.unifil.br>
2025-05-20 19:39:13 -0300

    Adicionando arquivos do projeto

---
+++ No/NO.java
@@ -0,0 +1,11 @@
+public class NO {
+    String valor;
+    
+    NO direita, esquerda;
+
+    public NO(String valor ) {
+        this.valor = valor;
+        direita = esquerda = null;
+    }
+    
+}
---
+++ No/public class Arvore {.java
@@ -0,0 +1,5 @@
+public class Arvore {
+
+    NO raiz;
+    
+}
